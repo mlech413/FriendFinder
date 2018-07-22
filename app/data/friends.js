c57aa1008d69abae3c2f3cb66e870386eaffadb7
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-22 12:01:13 -0400

    main

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        "name":"Will Smith",
+        "photo":"http://www.geronguide.com/gallery/var/albums/Attention-Deficit-Hyperactivity-Disorder/famous-people-and-adhd-24.jpg?m=1467944903",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name":"Mike Tyson",
+        "photo":"https://cdn.self-made.io/wp-content/uploads/2017/07/mike-tyson.jpg",
+        "scores":[
+            2,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            4,
+            1,
+            1
+          ]
+    },
+    {
+        "name":"Adam Sandler",
+        "photo":"https://www.thefamouspeople.com/profiles/images/adam-sandler-2.jpg",
+        "scores":[
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            4,
+            1
+          ]
+    }
+  ];
+
+  module.exports = friends;

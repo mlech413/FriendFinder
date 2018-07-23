5e34b593bdd93d7482c5121be1bdf556433fb7ac
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-23 10:05:53 -0400

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,6 +17,22 @@ var friends = [
           ]
     },
     {
+        "name":"Taylor Swift",
+        "photo":"https://pixel.nymag.com/imgs/fashion/daily/2018/05/30/30-taylor-swift.w710.h473.jpg",
+        "scores":[
+            "4",
+            "5",
+            "3",
+            "2",
+            "1",
+            "4",
+            "4",
+            "4",
+            "3",
+            "3"
+          ]
+    },
+    {
         "name":"Mike Tyson",
         "photo":"https://cdn.self-made.io/wp-content/uploads/2017/07/mike-tyson.jpg",
         "scores":[
@@ -30,6 +46,22 @@ var friends = [
             "4",
             "1",
             "1"
+          ]
+    },
+    {
+        "name":"Jennifer Aniston",
+        "photo":"https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2F1468010873%2F070816-make-up-eye-shape-3.jpg%3Fitok%3DX2oOkasD&w=800&q=85",
+        "scores":[
+            "3",
+            "3",
+            "5",
+            "2",
+            "4",
+            "3",
+            "2",
+            "4",
+            "1",
+            "2"
           ]
     },
     {
@@ -47,7 +79,56 @@ var friends = [
             "4",
             "1"
           ]
+    },
+    {
+        "name":"Mila Kunis",
+        "photo":"https://media.allure.com/photos/57718872a87d31b25a7e083b/3:4/w_767/beauty-trends-blogs-daily-beauty-reporter-mila-kunis-skin.jpg",
+        "scores":[
+            "1",
+            "3",
+            "1",
+            "5",
+            "4",
+            "4",
+            "5",
+            "2",
+            "3",
+            "2"
+          ]
+    },
+    {
+        "name":"Leonardo DiCaprio",
+        "photo":"https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/people/cover_images/leonardo_dicaprio-gt.jpg?itok=uZBLZv3X",
+        "scores":[
+            "2",
+            "4",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5",
+            "5",
+            "4",
+            "2"
+          ]
+    },
+    {
+        "name":"Jennifer Lawrence",
+        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Jennifer_Lawrence_SDCC_2015_X-Men.jpg/220px-Jennifer_Lawrence_SDCC_2015_X-Men.jpg",
+        "scores":[
+            "3",
+            "3",
+            "4",
+            "2",
+            "4",
+            "4",
+            "2",
+            "2",
+            "3",
+            "3"
+          ]
     }
+
   ];
 
   module.exports = friends;

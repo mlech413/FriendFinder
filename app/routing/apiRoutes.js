ad34763157ff14e8f8fea998f286c1d1a01c1b2f
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-24 10:08:19 -0400

    final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,13 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
-    console.log("apiRoutes");
     // API GET Request  
     app.get("/api/friends", function(req, res) {
-        console.log("get friends=" + friends);
         res.json(friends);
     });
-  
-  
+    
     // API POST Request
     app.post("/api/friends", function(req, res) {
-        console.log("post friends=" + friends);
         friends.push(req.body);
     });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,13 +6,11 @@ var path = require("path");
 module.exports = function(app) {
   // HTML GET Requests
   app.get("/survey", function(req, res) {
-      console.log("/survey received");
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    console.log("* received");
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
